1be065c202da6cf4ab715bfba9c68deb518ea9b4
Manjunath Biradar <manjunathsb10@gmail.com>
2023-07-18 17:05:03 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -9,3 +9,22 @@ const areaOfCircle = (radius, pi) => {
    return pi * radius * radius;
 };
 console.log("The area of the circle is: " + areaOfCircle(radius, pi));
+
+function func() { 
+         function fact(num) {     
+            if (num < 0){ 
+               return -1; 
+            } 
+            else if(num == 0){ 
+               return 1; 
+            } 
+            else { 
+               let result = 1; 
+               for(var i = num; i > 1; i--){ 
+                  result *= i; 
+               }; 
+               return result; 
+            } 
+         }; 
+         const _num = 4; 
+ fact(_num); 
