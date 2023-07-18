39ea66fa72a1706e3e1316ab4724f26bb0e7b69c
Manjunath Biradar <manjunathsb10@gmail.com>
2023-07-18 17:59:47 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,8 +1,36 @@
 console.log("Welcome");   
 
+
 console.log("testcd");
-let temp=10;
-var _temp=10;
+ let num1=5;
+let num2=10
+
+var res=num1+num2
+
+const num3;
+
+if(num3==5)
+{
+console.log('Equals');
+}
+
+function circle(Radius)
+{
+    this.Radius = Radius;
+  // area method
+    this.area = function () 
+    {
+        return Math.PI * this.Radius * this.Radius;
+    };
+  // perimeter method
+    this.perimeter = function ()
+    {
+        return 2*Math.PI*this.Radius;
+    };
+}
+var c = new circle(3);
+console.log('Area =', c.area().toFixed(2));
+console.log('perimeter =', c.perimeter().toFixed(2));
 const radius = 10;
 const pi = Math.PI;
 const areaOfCircle = (radius, pi) => {
